84d55919d3edfecfb8e8ebdcaf3bf1c66f54ac06
consollog <yash1007soni@gmail.com>
2024-06-18 01:04:05 +0530

    third

--- src/components/Application/Application.jsx
+++ src/components/Application/Application.jsx
@@ -35,7 +35,7 @@ const Application = () => {
 
     try {
       const { data } = await axios.post(
-        "https://job-app-backend-cbmx.onrender.com/api/v1/application/post",
+        "https://job-app-backend-1.onrender.com/api/v1/application/post",
         formData,
         {
           withCredentials: true,
--- src/components/Auth/Login.jsx
+++ src/components/Auth/Login.jsx
@@ -18,7 +18,7 @@ const Login = () => {
     e.preventDefault();
     try {
       const { data } = await axios.post(
-        "https://job-app-backend-cbmx.onrender.com/api/v1/user/login",
+        "https://job-app-backend-1.onrender.com/api/v1/user/login",
         { email, password, role },
         {
           headers: {
